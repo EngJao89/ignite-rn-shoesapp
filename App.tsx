a060e1b30323919ec092edbeaa5affa2f98c7e02
EngJao89 <eng_joao@outlook.com>
2023-06-10 23:42:06 -0300

    feat(is06-background-message): implementa primeira mensagem de push notification

--- App.tsx
+++ App.tsx
@@ -13,7 +13,11 @@ import { CartContextProvider } from './src/contexts/CartContext';
 const oneSignalAppId = Platform.OS === 'ios' ? '2b542c58-e2e9-48f1-8bff-004caefe40c4' : 'bb68b7c0-8d9c-40f2-80e9-20a02124c7ff';
 OneSignal.setAppId(oneSignalAppId);
 
-OneSignal.promptForPushNotificationsWithUserResponse()
+OneSignal.setEmail('joao@email.com')
+
+OneSignal.promptForPushNotificationsWithUserResponse(response => {
+  console.log(response);
+})
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
